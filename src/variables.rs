8895d6e4839c0e2cdb49ba693785039d49ee8701
Loic Diridollou <loic.diridollou@gmail.com>
2024-01-08 17:45:11 -0800

    Adding constant and shadowing

--- src/variables.rs
+++ src/variables.rs
@@ -1,5 +1,8 @@
 pub fn var_main() {
-    mutability()
+    mutability();
+    let val = constant();
+    println!("Constant value is {}", val);
+    shadowing();
 }
 
 fn mutability() {
@@ -10,4 +13,17 @@ fn mutability() {
     println!("The value of y is {}", y);
     y = 5;
     println!("The value of y is {}", y);
+}
+
+fn constant() -> u32 {
+    const TEST: u32 = 100_000;
+    return TEST;
+}
+
+fn shadowing() {
+    let x = 6;
+    println!("The value of x is {}", x);
+    // x = 5 would not work
+    let x = 6;
+    println!("The value of y is {}", x);
 }
