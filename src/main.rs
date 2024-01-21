1c961a8dae1c49af10f4afbec79966ea4560e0f0
Loic Diridollou <loic.diridollou@gmail.com>
2024-01-20 22:04:22 -0500

    Updating the main file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use control_flow::control_flow;
 use ownership::ownership_main;
 use scalar_datatypes::{compound_types, scalar_types};
+use slices::slices_main;
 use variables::variables_main;
 
 mod control_flow;
 mod ownership;
 mod scalar_datatypes;
+mod slices;
 mod variables;
 
 fn main() {
@@ -14,4 +16,5 @@ fn main() {
     compound_types();
     control_flow();
     ownership_main();
+    slices_main();
 }
