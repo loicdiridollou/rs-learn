b23bfce5f920423bf9fec2e0b582aae6250eab1e
Loic Diridollou <loic.diridollou@gmail.com>
2024-01-21 11:09:19 -0500

    Adding content on slices of integer

--- src/slices.rs
+++ src/slices.rs
@@ -1,4 +1,14 @@
 pub fn slices_main() {
+    slices_on_string();
+    slices_on_array();
+}
+
+fn slices_on_array() {
+    let a: [i8; 5] = [1, 2, 3, 4, 5];
+    println!("{:?}", &a[0..2]);
+}
+
+fn slices_on_string() {
     let mut word: String = String::from("Test value");
     let hello: &str = &word[0..4];
     let world: &str = &word[5..10];
