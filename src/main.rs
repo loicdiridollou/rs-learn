f97f458cd9a0bb85454233b588979a2a5cc9596e
Loic Diridollou <loic.diridollou@gmail.com>
2024-01-26 17:21:15 -0800

    Adding content on structs

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ use control_flow::control_flow;
 use ownership::ownership_main;
 use scalar_datatypes::{compound_types, scalar_types};
 use slices::slices_main;
+use structs::structs_main;
 use variables::variables_main;
 
 mod control_flow;
 mod ownership;
 mod scalar_datatypes;
 mod slices;
+mod structs;
 mod variables;
 
 fn main() {
@@ -17,4 +19,5 @@ fn main() {
     control_flow();
     ownership_main();
     slices_main();
+    structs_main();
 }
---
+++ src/structs.rs
@@ -0,0 +1,39 @@
+#[derive(Debug)]
+struct User {
+    user: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+pub fn structs_main() {
+    let user1 = User {
+        user: String::from("John"),
+        email: String::from("j.d@gmail.com"),
+        sign_in_count: 0,
+        active: false,
+    };
+
+    let name = user1.user;
+    println!("{}", name);
+    let email = user1.email;
+    println!("{}", email);
+    let sign_in_count = user1.sign_in_count;
+    println!("{}", sign_in_count);
+    let active = user1.active;
+    println!("{}", active);
+
+    println!(
+        "{:?}",
+        build_user(String::from("John"), String::from("j.d@gmail.com"))
+    );
+}
+
+fn build_user(user: String, email: String) -> User {
+    return User {
+        user,
+        email,
+        sign_in_count: 1,
+        active: true,
+    };
+}
