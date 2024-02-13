4ada68c33072e8bdfdbc11c8d066a85a303559b1
Loic Diridollou <loic.diridollou@gmail.com>
2024-02-12 17:53:55 -0800

    Conent for Option match

--- src/enums.rs
+++ src/enums.rs
@@ -45,4 +45,70 @@ fn main() {
     _message.change_color();
     Message::some_function();
     let _quit_message = Message::Quit;
+
+    // option enums
+    option_enum();
+
+    // match patterns
+    match_patterns();
+
+    // match on option enum
+    option_match_enum();
+}
+
+fn option_enum() {
+    let _some_num = Some(34);
+    let _some_string = Some("string");
+    let _some_empty: Option<i32> = None;
+
+    // summing optional and number
+    let num: i8 = 5;
+    let option_num: Option<i8> = Some(6);
+
+    println!("{}", num + option_num.unwrap_or(0));
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn match_patterns() {
+    let value = get_coin_value(&Coin::Dime);
+    println!("{}", value);
+
+    let value = get_coin_value(&Coin::Penny);
+    println!("{}", value);
+
+    let value = get_coin_value(&Coin::Nickel);
+    println!("{}", value);
+
+    let value = get_coin_value(&Coin::Quarter);
+    println!("{}", value);
+}
+
+fn get_coin_value(coin: &Coin) -> i32 {
+    let value = match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    };
+    return value;
+}
+
+fn option_match_enum() {
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("{:?}", six);
+    println!("{:?}", five);
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
 }
