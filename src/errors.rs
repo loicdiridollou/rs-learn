7c49122712f4fba20662daf3261f24ba4f1404b9
Loic Diridollou <loic.diridollou@gmail.com>
2024-02-15 21:16:28 -0800

    adding content on error

--- src/errors.rs
+++ src/errors.rs
@@ -1,15 +1,42 @@
 // error handling in Rust
 
-use std::{fs::File, io::ErrorKind};
+use std::io::ErrorKind;
+use std::{
+    fs::File,
+    io::{self, Read},
+};
 
 fn main() {
-    // panic!("This is an error crashing the program")
+    // panic!("This is an error crashicag the program")
 
-    enum _Result<T, E> {
-        Ok(T),
-        Err(E),
+    error_propagation();
+    error_handling_file();
+}
+
+fn error_propagation() {
+    match read_from_file() {
+        Ok(_) => println!("Read the file"),
+        Err(e) => println!("{:?}", e),
     }
+}
 
+fn read_from_file() -> Result<String, io::Error> {
+    let f_result = File::open("hello.txt");
+
+    let mut f = match f_result {
+        Ok(file) => file,
+        Err(e) => return Err(e),
+    };
+
+    let mut s: String = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+fn error_handling_file() {
     let file = File::open("hello.txt");
     let _f = match file {
         Ok(file) => file,
@@ -23,4 +50,11 @@ fn main() {
             }
         },
     };
+    let _file = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| panic!("{:?}", error))
+        } else {
+            panic!("{:?}", error);
+        }
+    });
 }
