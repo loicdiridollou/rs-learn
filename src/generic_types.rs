b498f54c06cffaba6948ed5286c7a176d8da0981
Loic Diridollou <loic.diridollou@gmail.com>
2024-02-17 13:10:44 -0800

    adding content on generic types

---
+++ src/generic_types.rs
@@ -0,0 +1,31 @@
+// generic types in Rust
+
+fn main() {
+    let list_nums = vec![1, 2, 3, 695, 35];
+    println!("{}", largest_num(&list_nums));
+
+    let list_char = vec!['y', 'm', 'a', 'q'];
+    println!("{}", largest_char(&list_char));
+}
+
+fn largest_num(s: &[i32]) -> &i32 {
+    let mut largest: &i32 = &s[0];
+
+    for item in s {
+        if item > largest {
+            largest = item
+        }
+    }
+    return largest;
+}
+
+fn largest_char(s: &[char]) -> &char {
+    let mut largest: &char = &s[0];
+
+    for item in s {
+        if item > largest {
+            largest = item
+        }
+    }
+    return largest;
+}
