2b4f8d04cad565b7522caf167ae76f194bd328da
Loic Diridollou <loic.diridollou@gmail.com>
2024-02-17 16:16:29 -0800

    Adding concent on generics

--- src/generic_types.rs
+++ src/generic_types.rs
@@ -6,6 +6,12 @@ fn main() {
 
     let list_char = vec!['y', 'm', 'a', 'q'];
     println!("{}", largest_char(&list_char));
+
+    let list_nums = vec![1, 2, 3, 695, 35];
+    println!("{}", largest(&list_nums));
+
+    let list_char = vec!['y', 'm', 'a', 'q'];
+    println!("{}", largest(&list_char));
 }
 
 fn largest_num(s: &[i32]) -> &i32 {
@@ -21,6 +27,17 @@ fn largest_num(s: &[i32]) -> &i32 {
 
 fn largest_char(s: &[char]) -> &char {
     let mut largest: &char = &s[0];
+
+    for item in s {
+        if item > largest {
+            largest = item
+        }
+    }
+    return largest;
+}
+
+fn largest<T: PartialOrd>(s: &[T]) -> &T {
+    let mut largest: &T = &s[0];
 
     for item in s {
         if item > largest {
