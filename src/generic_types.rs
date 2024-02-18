3f39999163ab5638ed3f4df576297b902399d3c9
Loic Diridollou <loic.diridollou@gmail.com>
2024-02-17 16:53:58 -0800

    adding generic enums

--- src/generic_types.rs
+++ src/generic_types.rs
@@ -1,5 +1,7 @@
 // generic types in Rust
 
+use std::fmt;
+
 fn main() {
     let list_nums = vec![1, 2, 3, 695, 35];
     println!("{}", largest_num(&list_nums));
@@ -12,6 +14,20 @@ fn main() {
 
     let list_char = vec!['y', 'm', 'a', 'q'];
     println!("{}", largest(&list_char));
+
+    let coordinate = Coord { x: 1, y: 2 };
+    println!("{}", coordinate);
+}
+
+struct Coord<T> {
+    x: T,
+    y: T,
+}
+
+impl<T: fmt::Display> fmt::Display for Coord<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Coordinate with x: {} and x: {}", &self.x, &self.y)
+    }
 }
 
 fn largest_num(s: &[i32]) -> &i32 {
