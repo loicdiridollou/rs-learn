c2f458c0ec26306121937323c71729c5f92a8946
Loic Diridollou <loic.diridollou@gmail.com>
2024-02-23 13:25:52 -0600

    Adding traits content

--- src/traits.rs
+++ src/traits.rs
@@ -1,5 +1,7 @@
 // Chapter on traits
 
+use std::fmt::Display;
+
 pub struct NewsArticle {
     pub headline: String,
     pub location: String,
@@ -22,6 +24,17 @@ trait Summary {
     }
 }
 
+impl Display for NewsArticle {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.author, self.author)
+    }
+}
+impl Display for Tweet {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {}", self.username, self.content)
+    }
+}
+
 impl Summary for NewsArticle {
     fn summarize(&self) -> String {
         format!("{}: {}", self.headline, self.author)
@@ -38,6 +51,32 @@ impl Summary for Tweet {
     }
     fn summarize(&self) -> String {
         format!("{}: {}", self.username, self.content)
+    }
+}
+
+fn notify<T: Summary + Display>(item: &T) {
+    println!("Breaking news! {}", item);
+    println!("Breaking news! {}", item.summarize());
+}
+
+fn returns_summarizable(switch: bool) -> Box<dyn Summary> {
+    if switch {
+        Box::new(NewsArticle {
+            headline: String::from("Penguins win the Stanley Cup Championship!"),
+            location: String::from("Pittsburgh, PA, USA"),
+            author: String::from("Iceburgh"),
+            content: String::from(
+                "The Pittsburgh Penguins once again are the best \
+                 hockey team in the NHL.",
+            ),
+        })
+    } else {
+        Box::new(Tweet {
+            username: String::from("horse_ebooks"),
+            content: String::from("of course, as you probably already know, people"),
+            reply: false,
+            retweet: false,
+        })
     }
 }
 
@@ -62,4 +101,8 @@ fn main() {
     };
 
     println!("New article available! {}", article.summarize());
+    notify(&article);
+    notify(&tweet);
+
+    println!("{}", returns_summarizable(true).summarize())
 }
