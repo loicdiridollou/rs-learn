c9bcaccfb49e814b872a530a1fbb6c593e43b23a
Loic Diridollou <loic.diridollou@gmail.com>
2024-03-07 07:53:17 -0800

    Adding new content to minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,6 +1,22 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+
+    read_file_content(file_path);
+}
+
+fn read_file_content(file_path: &str) {
+    // --snip--
+    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("With text:\n{contents}");
 }
