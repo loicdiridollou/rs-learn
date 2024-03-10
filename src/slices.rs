b9df3499551a01e6647bc2bedb847a10bbb6e3a4
Loic Diridollou <loic.diridollou@gmail.com>
2024-03-10 16:49:15 -0700

    Adding test content on vector access

--- src/slices.rs
+++ src/slices.rs
@@ -1,6 +1,34 @@
 pub fn slices_main() {
     slices_on_string();
     slices_on_array();
+    squares_of_sorted();
+}
+
+fn squares_of_sorted() {
+    let values = Vec::<i32>::from([-9, -4, 0, 1, 10]);
+    let (mut l, mut r): (usize, usize) = (0, values.len() - 1);
+    let mut new_vec: Vec<i32> = vec![0; values.len()];
+    let mut i = values.len() - 1;
+
+    while l <= r {
+        if values[l].pow(2) > values[r].pow(2) {
+            new_vec[i] = values[l].pow(2);
+            l += 1;
+            if i == 0 {
+                break;
+            }
+            i -= 1
+        } else {
+            new_vec[i] = values[r].pow(2);
+            r -= 1;
+            if i == 0 {
+                break;
+            }
+            i -= 1;
+        }
+    }
+
+    println!("{:?}", new_vec);
 }
 
 fn slices_on_array() {
@@ -29,7 +57,7 @@ fn first_word(s: &String) -> usize {
         }
     }
 
-    return s.len();
+    s.len()
 }
 
 fn first_word_str(s: &str) -> usize {
