4b4a72cdb76221e179ac34f44837a23a011107b4
Loic Diridollou <loic.diridollou@gmail.com>
2024-03-22 20:52:36 -0700

    Adding content on closures

---
+++ src/closures.rs
@@ -0,0 +1,12 @@
+use std::thread;
+use std::time::Duration;
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly ...");
+    thread::sleep(Duration::from_secs(2));
+    return intensity;
+}
+
+fn main() {
+    println!("Intensity is {}", simulated_expensive_calculation(23));
+}
